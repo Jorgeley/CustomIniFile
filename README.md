373be7be8dbadea62035a536fb918de04539b0b9
jorgeley <jorgeley@gmail.com>
2017-09-16 16:22:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Build you own ini file
+
+#I googled around and found this sample very interesting and useful
+#many thanks to Everton Silva https://github.com/EvertonSilva
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// read the .ini file and create an associative array
+
+$db = parse_ini_file("config.ini");
+
+/* now we can use the info in that file to create the appropriate, string connection, based on type, or just using the other info */
+
+$user = $db['user'];
+$pass = $db['pass'];
+$name = $db['name'];
+$host = $db['host'];
+$type = $db['type'];
+
+echo "$user\n";
+echo "$pass\n";
+echo "$name\n";
+echo "$host\n";
+echo "$type\n";
